f437c7f6a8c8e4193aa442c1b843c5e282a89989
Sreedev-p <sreedevvly@gmail.com>
2026-08-29 16:09:44 +0530

    Update proxy and backend URLs in app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -152,8 +152,8 @@ st.markdown(f"""
 st.title("🛡️ DataExpiry: Zero-Code Cryptographic Erasure")
 
 # dashboard/app.py (around line 8)
-PROXY_URL = "https://53f21cdbbea6ac.lhr.life"
-BACKEND_URL = "https://20c34147519051.lhr.life"
+PROXY_URL = "https://e17e0eb2f16b0f.lhr.life"
+BACKEND_URL = "https://ab39cc9e6168a0.lhr.life"
 
 # =========================================================
 # SIDEBAR: Enterprise DLP Admin Config Panel
