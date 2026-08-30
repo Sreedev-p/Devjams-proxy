000da30f4cfdaa12153c9a8ffa4577ca05968f45
Sreedev-p <sreedevvly@gmail.com>
2026-08-30 10:02:13 +0530

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 
 PROXY_URL = "https://latrine-primal-retired.ngrok-free.dev"
-BACKEND_URL = "https://thirty-plants-boil.loca.lt"
+BACKEND_URL = "https://shaky-jokes-lick.loca.lt"
 
 TUNNEL_HEADERS = {
     "Bypass-Tunnel-Reminder": "true",
