a19c8dac1a4d258d8dfdfa6885043460d4ff9a27
Sreedev-p <sreedevvly@gmail.com>
2026-08-30 10:11:22 +0530

    Update TARGET_BACKEND URL in proxy.py

--- proxy.py
+++ proxy.py
@@ -13,7 +13,7 @@ import vault
 import crypto_engine
 from reaper import run_reaper
 
-TARGET_BACKEND = os.getenv("TARGET_BACKEND", "https://shaky-jokes-lick.loca.lt")
+TARGET_BACKEND = os.getenv("TARGET_BACKEND", "https://large-geese-live.loca.lt")
 DEFAULT_TARGET_FIELDS = {"sensitive_data"}
 ADMIN_API_KEY = os.getenv("DATAEXPIRY_ADMIN_KEY", "hackathon_admin_99")
 
